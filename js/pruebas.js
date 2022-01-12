f3bc0f83c7b5f2b2bc88161c41173daca12cb759
estebankrzak <50186552+estebankrzak@users.noreply.github.com>
2022-01-12 19:58:29 -0300

    Agrega pruebas de casos positivos.

--- js/pruebas.js
+++ js/pruebas.js
@@ -8,6 +8,16 @@ function probarValidarNombre() {
         validarNombre('111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
             'Validar nombre no validó que el nombre sea menor a 50 caracteres',
     );
+
+    console.assert(
+        validarNombre('321321dsad') === 'Este campo sólo debe contener letras',
+        'La función validarNombre no validó que el nombre contenga sólo letras.',
+    );
+
+    console.assert(
+        validarNombre('Juan') === '',
+        'La función validarNombre debería retornar un string vacío cuando se le ingresa un nombre correcto.'
+    );
 }
 
 
@@ -15,6 +25,11 @@ function probarValidarCiudad() {
     console.assert(
         validarCiudad('') === 'Debes elegir alguna ciudad.',
         'Validar ciudad no validó que la ciudad haya sido seleccionada.',
+    );
+
+    console.assert(
+        validarCiudad('Rosario') === '',
+        'validarCiudad debería retornar un string vacío cuando se selecciona una ciudad correcta'
     );
 
 }
@@ -25,7 +40,25 @@ function probarValidarDescripcionRegalo() {
         'Validar descripcion regalo no valido que el texto no sea vacio,'
     );
 
+    console.assert(
+        validarDescripcionRegalo('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaagit') === 
+        'La descripción del regalo debe ser menor a 100 caracteres.',
+        'Validar descripción regalo no validó que el texto tenga menos de 100 caracteres'
+    );
+
+    console.assert(
+        validarDescripcionRegalo('eeeeeeee!dsad# ') === 'La descripción del regalo sólo debe tener letras y números.',
+        'Validar descripcionRegalo no validó que el texto no contenga caracteres especiales',
+    );
+
+    console.assert(
+        validarDescripcionRegalo('Regalo rojo especial') === '',
+        'La función validarDescripcionRegalo debería retornar un string vacío cuando se ingresa una descripción correcta.'
+    );
+
 }
+
+
 probarValidarNombre();
 probarValidarCiudad();
 probarValidarDescripcionRegalo();
