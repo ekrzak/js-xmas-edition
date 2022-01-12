18590dca3bee64a0c196168c50d95737fea4ce3b
estebankrzak <50186552+estebankrzak@users.noreply.github.com>
2022-01-12 14:19:21 -0300

    Añade funciones para validar formulario y manejo de errores

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const $form = document.querySelector('#carta-a-santa');
 
 const nombre = $form.nombre.value;
 const ciudad = $form.ciudad.value;
-const $descripcionRegalo = document.querySelector('[name=descripcion-regalo]');
+const descripcionRegalo = $form['descripcion-regalo'].value;
 
 function validarNombre(nombre) {
     if (nombre.length === 0) {
@@ -19,6 +19,10 @@ function validarNombre(nombre) {
 
     if (nombre.length >= 50) {
         return 'Este campo debe tener menos de 50 caracteres';
+    }
+
+    if (!/^[A-z]+$/i.test(nombre)) {
+        return 'Este campo sólo debe contener letras';
     }
 
     return '';
@@ -36,14 +40,56 @@ function validarDescripcionRegalo(descripcion) {
     if (descripcion.length === 0) {
         return 'Debes escribir una descripcion del regalo que quieres.';
     }
+    if (descripcion.length >= 100) {
+        return 'La descripción del regalo debe ser menor a 100 caracteres.'
+    }
+    if (!/^[a-z 0-9]+$/i.test(descripcion)) {
+        return 'La descripción del regalo sólo debe tener letras y números.'
+    }
 
     return '';
 }
 
-document.querySelector('#enviar-carta').onclick = function(event) {
-    validarNombre(nombre)
-    validarCiudad(ciudad);
-    validarDescripcionRegalo($descripcionRegalo);
+function validarForm(event) {
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+    const errores = {
+        nombre: validarNombre(nombre),
+        ciudad: validarCiudad(ciudad),
+        'descripcion-regalo': validarDescripcionRegalo(descripcionRegalo)
+    }; 
     
+    manejarErrores(errores);
+
     event.preventDefault();
-};
+}
+
+function manejarErrores(errores) {
+    let cantidadErrores = 0;
+
+    Object.keys(errores).forEach(function(key) {
+        $form[key].classList.remove('error');
+        document.querySelector(`#error-${key}`).classList.add('oculto');
+
+        if (errores[key].length) {
+            cantidadErrores++;
+            $form[key].classList.add('error');
+            $form[key].value = '';
+            document.querySelector(`#error-${key}`).innerText = errores[key];
+            document.querySelector(`#error-${key}`).classList.remove('oculto');
+        }
+
+    });
+
+    if (!cantidadErrores) {
+        $form.classList.add('oculto');
+        document.querySelector('#exito').classList.remove('oculto');
+        window.setTimeout(function() {
+            window.location.href = `C:/Users/Esteban/Desktop/WebDev/r-argentinaprograma/js-xmas-edition/wishlist.html`;
+        }, 5000);
+    }
+    
+}
+
+$form.onsubmit = validarForm;
